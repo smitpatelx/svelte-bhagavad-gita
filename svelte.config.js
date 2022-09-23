0685c2e795af4f4bee0e91d1bd0c24ed86cdc90a
smitpatelx <smitbpatel1999@gmail.com>
2022-09-22 23:39:08 -0500

    Adapter vercel removed

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,11 +10,7 @@ const config = {
   }),
 
 	kit: {
-		adapter: adapter({
-      edge: false,
-      split: false,
-      external: [],
-    }),
+		adapter: adapter(),
     prerender: {
       default: true,
     },
